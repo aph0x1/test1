0d3208d83dbb030883a152b2eb74762f9b24a076
aph0x1 <aph0x1@gmail.com>
2012-09-02 17:25:54 -0300

    Update a.js

--- a.js
+++ a.js
@@ -207,6 +207,11 @@ function f_ron(data){
                         API.sendChat(data.from + ", Se ha tomado un vaso de Ron, Yoho! Yoho! que corra el ron!!!");
 }
 
+function f_reload(data) {
+		API.sendChat('/me Recargando..');
+		window.setTimeout(function(){location.reload();}, 1000);
+}
+
 var o_chatcmds = {
         '/comandos': {
                 f: f_comandos,
@@ -279,6 +284,10 @@ var o_chatcmds = {
         '/ron': {
                 f: f_ron,
                 needsPerm: false
+        },
+        '/recargar': {
+                f: f_reload,
+                needsPerm: true
         },
 };
 
