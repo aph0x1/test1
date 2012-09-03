119305ca14749d4b0bcd09dec167e778e2178309
aph0x1 <aph0x1@gmail.com>
2012-09-03 01:34:37 -0300

    Update a.js

--- a.js
+++ a.js
@@ -209,6 +209,14 @@ function f_ron(data){
 
 
 
+(function() {
+  var Command, cervezaCommand,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+
 
 var Command;
 
@@ -243,10 +251,10 @@ Command = (function() {
 })();
 
     
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-
     
+
+
+
   cervezaCommand = (function(_super) {
 
     __extends(cervezaCommand, _super);
